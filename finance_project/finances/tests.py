a252b1bc30e050401b8c9078440a9edf60f157e6
VenkataSwathi-M <swathi22092004@gmail.com>
2025-10-24 00:43:41 +0530

    Add test for creating Expense model
    Added a basic unit test in tests.py to verify that an Expense object can be created successfully with a given name and amount.

--- finance_project/finances/tests.py
+++ finance_project/finances/tests.py
@@ -1,3 +1,7 @@
 from django.test import TestCase
+from .models import Expense
 
-# Create your tests here.
+class ExpenseModelTest(TestCase):
+    def test_create_expense(self):
+        exp = Expense.objects.create(name="Food", amount=200)
+        self.assertEqual(exp.amount, 200)
